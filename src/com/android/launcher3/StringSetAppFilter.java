e2f68b91e95cf63efed87b0f0111b4a96982fb75
ezio84 <brabus84@gmail.com>
2018-01-29 20:59:12 +0100

    Hide only default hidden apps in the widget panel, not user ones
    Change-Id: Id455290ad2169c2d3311df646dad57a550bfb9cc
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/launcher3/StringSetAppFilter.java
+++ src/com/android/launcher3/StringSetAppFilter.java
@@ -8,9 +8,12 @@ import java.util.Set;
 public class StringSetAppFilter implements AppFilter {
 
     @Override
-    public boolean shouldShowApp(String packageName, Context context) {
-
+    public boolean shouldShowApp(String packageName, Context context, boolean isWidgetPanel) {
         Set<String> hiddenApps = PreferenceManager.getDefaultSharedPreferences(context).getStringSet(Utilities.KEY_HIDDEN_APPS_SET, null);
-        return hiddenApps == null || !hiddenApps.contains(packageName);
+
+        if (isWidgetPanel) {
+            return !mBlackList.contains(packageName);
+        }
+        return !mBlackList.contains(packageName) && (hiddenApps == null || !hiddenApps.contains(packageName));
     }
 }
