omnirom/android_packages_apps_Launcher3

Launcher3: add theme style switch
additional to automatic wallpaper based allow
setting dedicated light or dark style

Change-Id: I70435beda7d4b5a4e2b84fe33bb277c4e3356194

src/com/android/launcher3/BaseDraggingActivity.java
@@ -65,6 +65,8 @@ public abstract class BaseDraggingActivity extends BaseActivity
 
     private DisplayRotationListener mRotationListener;
 
+    protected int mThemeStyle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,12 +91,18 @@ public abstract class BaseDraggingActivity extends BaseActivity
     }
 
     protected int getThemeRes(WallpaperColorInfo wallpaperColorInfo) {
-        if (wallpaperColorInfo.isDark()) {
-            return wallpaperColorInfo.supportsDarkText() ?
-                    R.style.LauncherThemeDark_DarKText : R.style.LauncherThemeDark;
+        if (mThemeStyle == 1) {
+            return R.style.LauncherTheme;
+        } else if (mThemeStyle == 2) {
+            return R.style.LauncherThemeDark;
         } else {
-            return wallpaperColorInfo.supportsDarkText() ?
-                    R.style.LauncherTheme_DarkText : R.style.LauncherTheme;
+            if (wallpaperColorInfo.isDark()) {
+                return wallpaperColorInfo.supportsDarkText() ?
+                        R.style.LauncherThemeDark_DarKText : R.style.LauncherThemeDark;
+            } else {
+                return wallpaperColorInfo.supportsDarkText() ?
+                        R.style.LauncherTheme_DarkText : R.style.LauncherTheme;
+            }
         }
     }
 

Colt-Enigma/platform_packages_apps_Launcher3

Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/launcher3/BaseDraggingActivity.java
@@ -66,6 +66,7 @@ public abstract class BaseDraggingActivity extends BaseActivity
     private DisplayRotationListener mRotationListener;
 
     private UiModeManager mUiModeManager;
+    protected int mThemeStyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,14 +94,21 @@ public abstract class BaseDraggingActivity extends BaseActivity
     }
 
     protected int getThemeRes(WallpaperColorInfo wallpaperColorInfo) {
-        if (wallpaperColorInfo.isDark()) {
-            return wallpaperColorInfo.supportsDarkText() ?
-                    R.style.AppTheme_Dark_DarkText : R.style.AppTheme_Dark;
+        if (mThemeStyle == 1) {
+            return R.style.LauncherTheme;
+        } else if (mThemeStyle == 2) {
+            return R.style.AppTheme_Dark;
         } else {
-            return wallpaperColorInfo.supportsDarkText() ?
-                    R.style.AppTheme_DarkText : R.style.AppTheme;
+            if (wallpaperColorInfo.isDark()) {
+                return wallpaperColorInfo.supportsDarkText() ?
+                        R.style.AppTheme_Dark_DarkText : R.style.AppTheme_Dark;
+            } else {
+                return wallpaperColorInfo.supportsDarkText() ?
+                        R.style.AppTheme_DarkText : R.style.AppTheme;
+            }
         }
     }
+
 
     @Override
     public void onActionModeStarted(ActionMode mode) {