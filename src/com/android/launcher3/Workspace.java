ezio84/abc_packages_apps_Launcher3

Add a flashlight gesture & group it with the sleep one
This adds a flashlight double tap gesture, using the existing
model laid out Thecrazyskull/Ezio. Also opens the door to add in more
options into the case switch in the event that someone wants to
add something else (screenshot, hotspot, etc).

Change-Id: Iba1458f2988259a3448490c67a238cd13940e900

src/com/android/launcher3/Workspace.java
@@ -36,16 +36,21 @@ import android.app.WallpaperManager;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.hardware.camera2.CameraManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Parcelable;
 import android.os.UserHandle;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -100,6 +105,8 @@ import com.android.launcher3.widget.PendingAppWidgetHostView;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.android.launcher3.Utilities.getDevicePrefs;
 
 /**
  * The workspace is a wide area with a wallpaper and a finite number of pages.
@@ -251,6 +258,11 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
     private final WorkspaceStateTransitionAnimation mStateTransitionAnimation;
 
     private GestureDetector mGestureListener;
+    private CameraManager cameraManager;
+
+    // Used to determine camera state
+    private boolean flashLightStatus = false;
+    private int mGestureMode;
 
     /**
      * Used to inflate the Workspace from XML.
@@ -286,11 +298,13 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
         context.enforceCallingOrSelfPermission(
                     android.Manifest.permission.DEVICE_POWER, null);
+        mGestureMode = Integer.valueOf(
+                getDevicePrefs(getContext()).getString("pref_homescreen_dt_gestures", "0"));
         mGestureListener =
                 new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent event) {
-                Utils.switchScreenOff(context);
+                triggerGesture(event);
                 return true;
             }
 
@@ -313,6 +327,33 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         });
 
         setOnTouchListener(new WorkspaceTouchListener(mLauncher, this));
+    }
+
+    private void triggerGesture(MotionEvent event) {
+        switch(mGestureMode) {
+            // Stock behavior
+            case 0:
+                break;
+            // Sleep
+            case 1:
+                Utils.switchScreenOff(getContext());
+                break;
+            // Flashlight
+            case 2:
+                flashLight();
+                break;
+        }
+    }
+
+    public void setGestures(int mode) {
+        mGestureMode = mode;
+    }
+
+    private void flashLight() {
+        if (getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            Torch Torch = new Torch(getContext());
+            Torch.execute();
+        }
     }
 
     private boolean openNotifications() {
@@ -3502,6 +3543,34 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         @Override
         public void onAnimationEnd(Animator animation) {
             onEndStateTransition();
+        }
+    }
+
+    private class Torch extends AsyncTask {
+
+        Torch(Context ctx) {
+            cameraManager = (CameraManager) ctx.getSystemService(Context.CAMERA_SERVICE);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            if (flashLightStatus) {
+                try {
+                    cameraManager.setTorchMode(cameraManager.getCameraIdList()[0], true);
+                    flashLightStatus = false;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    cameraManager.setTorchMode(cameraManager.getCameraIdList()[0], false);
+                    flashLightStatus = true;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
         }
     }
 }

Colt-Enigma/platform_packages_apps_Launcher3

src/com/android/launcher3/Workspace.java
@@ -101,6 +101,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.android.launcher3.Utilities.getDevicePrefs;
+
 /**
  * The workspace is a wide area with a wallpaper and a finite number of pages.
  * Each page contains a number of icons, folders or widgets the user can
@@ -251,6 +253,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
     private final WorkspaceStateTransitionAnimation mStateTransitionAnimation;
 
     private GestureDetector mGestureListener;
+    private int mGestureMode;
 
     /**
      * Used to inflate the Workspace from XML.
@@ -286,11 +289,13 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
         context.enforceCallingOrSelfPermission(
                     android.Manifest.permission.DEVICE_POWER, null);
+        mGestureMode = Integer.valueOf(
+                getDevicePrefs(getContext()).getString("pref_homescreen_dt_gestures", "0"));
         mGestureListener =
                 new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent event) {
-                ColtUtils.switchScreenOff(context);
+                triggerGesture(event);
                 return true;
             }
 
@@ -313,6 +318,26 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         });
 
         setOnTouchListener(new WorkspaceTouchListener(mLauncher, this));
+    }
+
+    private void triggerGesture(MotionEvent event) {
+        switch(mGestureMode) {
+            // Stock behavior
+            case 0:
+                break;
+            // Sleep
+            case 1:
+                ColtUtils.switchScreenOff(getContext());
+                break;
+            // Flashlight
+            case 2:
+                ColtUtils.toggleCameraFlash();
+                break;
+        }
+    }
+
+    public void setGestures(int mode) {
+        mGestureMode = mode;
     }
 
     private boolean openNotifications() {