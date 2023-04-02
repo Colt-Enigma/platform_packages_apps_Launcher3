yaap/packages_apps_Launcher3

Launcher3: Don't discard custom grid on config changes
Change-Id: Ibfa2eafec50c9fe79d1e9594ddc68fb5266ecb18
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -343,7 +343,7 @@ public class InvariantDeviceProfile {
 
         // Re-init grid
         String gridName = getCurrentGridName(context);
-        initGrid(context, gridName);
+        initGrid(context, Utilities.getPrefs(context).getString(KEY_IDP_GRID_NAME, gridName));
 
         int changeFlags = 0;
         if (numRows != oldProfile.numRows ||

Colt-Enigma/platform_packages_apps_Launcher3

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -470,7 +470,7 @@ public class InvariantDeviceProfile {
 
         // Re-init grid
         String gridName = getCurrentGridName(context);
-        initGrid(context, gridName);
+        initGrid(context, LauncherPrefs.getPrefs(context).getString(KEY_IDP_GRID_NAME, gridName));
 
         boolean modelPropsChanged = !Arrays.equals(oldState, toModelState());
         for (OnIDPChangeListener listener : mChangeListeners) {